f09cf1fcb862ad451d85762b65abcd2dadd3610e
madeleinebennett4 <madeleinebennett4@gmail.com>
2023-08-06 22:48:53 +0100

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes} `;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector(".current-temp");
@@ -5,11 +28,13 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector(".humidity");
   let detailsElement = document.querySelector(".cloudiness");
   let windElement = document.querySelector(".wind");
+  let dateElement = document.querySelector(".current-date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   detailsElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "4cd9acb8c28909bac787b5e975b439ef";
