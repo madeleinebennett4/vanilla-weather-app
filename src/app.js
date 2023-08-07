73c004a3d5f7ef07323af02431f1b1cf5ccbf659
madeleinebennett4 <madeleinebennett4@gmail.com>
2023-08-07 23:00:15 +0100

    updated weather icon

--- src/app.js
+++ src/app.js
@@ -22,22 +22,27 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector(".current-temp");
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector(".humidity");
   let detailsElement = document.querySelector(".cloudiness");
   let windElement = document.querySelector(".wind");
   let dateElement = document.querySelector(".current-date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   detailsElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "4cd9acb8c28909bac787b5e975b439ef";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Crete&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
