04eb207073d882f97bf6a09ebee63ee8f66013e4
madeleinebennett4 <madeleinebennett4@gmail.com>
2023-08-07 23:21:45 +0100

    adjusted search engine

--- src/app.js
+++ src/app.js
@@ -42,7 +42,19 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "4cd9acb8c28909bac787b5e975b439ef";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Crete&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "4cd9acb8c28909bac787b5e975b439ef";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
